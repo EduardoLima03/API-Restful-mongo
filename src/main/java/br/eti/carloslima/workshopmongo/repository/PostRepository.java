5f69cbc63a9140917a2f2c1959819ae7a26c91f2
Carlos Lima <tec.carlos.lima@gmail.com>
2021-09-29 16:02:18 -0300

    Consulta simples com @Query

--- src/main/java/br/eti/carloslima/workshopmongo/repository/PostRepository.java
+++ src/main/java/br/eti/carloslima/workshopmongo/repository/PostRepository.java
@@ -3,13 +3,20 @@ package br.eti.carloslima.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.eti.carloslima.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-	
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	/*
+	 * metodo personalizado de realiza a cunsulta por titulo
+	 */
+	@Query("{ 'title': { $regex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
 
 }
