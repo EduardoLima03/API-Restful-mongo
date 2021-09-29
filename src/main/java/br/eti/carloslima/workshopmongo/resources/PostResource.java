740c0c982de0ba186b6d1c6d13c3f7b75b9647d8
Carlos Lima <tec.carlos.lima@gmail.com>
2021-09-29 15:14:34 -0300

    Obtendo um post por id

--- src/main/java/br/eti/carloslima/workshopmongo/resources/PostResource.java
+++ src/main/java/br/eti/carloslima/workshopmongo/resources/PostResource.java
@@ -1,20 +1,13 @@
 package br.eti.carloslima.workshopmongo.resources;
 
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.eti.carloslima.workshopmongo.domain.Post;
-import br.eti.carloslima.workshopmongo.dto.PostDTO;
 import br.eti.carloslima.workshopmongo.services.PostService;
 
 @RestController
@@ -24,52 +17,23 @@ public class PostResource {
 	@Autowired
 	private PostService service;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<PostDTO>> findAll() {
-
-		List<Post> list = service.findAll();
-
-		// convertendo a lista de user para uma dto
-		List<PostDTO> listDTO = list.stream().map(x -> new PostDTO(x)).collect(Collectors.toList());
-
-		return ResponseEntity.ok().body(listDTO);
-	}
+	/*
+	 * @RequestMapping(method = RequestMethod.GET) public
+	 * ResponseEntity<List<PostDTO>> findAll() {
+	 * 
+	 * List<Post> list = service.findAll();
+	 * 
+	 * // convertendo a lista de user para uma dto List<PostDTO> listDTO =
+	 * list.stream().map(x -> new PostDTO(x)).collect(Collectors.toList());
+	 * 
+	 * return ResponseEntity.ok().body(listDTO); }
+	 */
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<PostDTO> findById(@PathVariable String id) {
+	public ResponseEntity<Post> findById(@PathVariable String id) {
 
 		Post obj = service.findById(id);
 
-		return ResponseEntity.ok().body(new PostDTO(obj));
-	}
-
-	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> findById(@RequestBody PostDTO objDto) {
-
-		Post obj = service.fromDTO(objDto);
-		obj = service.insert(obj);
-
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).build();
-		// comando retorna uma resposta vazia com o codigo 201 e cabeçario contendo a
-		// localizaçao do obj criado
-	}
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable String id) {
-
-		service.delete(id);
-
-		return ResponseEntity.noContent().build();
-	}
-	
-	@RequestMapping(value= "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> findById(@RequestBody PostDTO objDto, @PathVariable String id) {
-
-		Post obj = service.fromDTO(objDto);
-		obj.setId(id);
-		obj = service.Update(obj);
-
-		return ResponseEntity.noContent().build();
+		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/br/eti/carloslima/workshopmongo/services/PostService.java
+++ src/main/java/br/eti/carloslima/workshopmongo/services/PostService.java
@@ -1,13 +1,11 @@
 package br.eti.carloslima.workshopmongo.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.eti.carloslima.workshopmongo.domain.Post;
-import br.eti.carloslima.workshopmongo.dto.PostDTO;
 import br.eti.carloslima.workshopmongo.repository.PostRepository;
 import br.eti.carloslima.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -17,41 +15,10 @@ public class PostService {
 	@Autowired
 	private PostRepository repo;
 
-	public List<Post> findAll() {
-		return repo.findAll();
-	}
+	// public List<Post> findAll() { return repo.findAll(); }
 
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-	}
-
-	public Post insert(Post obj) {
-		return repo.insert(obj);
-	}
-
-	public void delete(String id) {
-		findById(id);
-		repo.deleteById(id);
-	}
-
-	public Post Update(Post obj) {
-		Post newObj = findById(obj.getId());// objeto original vindo do banco de dados
-
-		updateData(newObj, obj);
-		return repo.save(newObj);
-	}
-
-	private void updateData(Post newObj, Post obj) {
-		// String id, Date date, String title, String boby, User author
-		newObj.setDate(obj.getDate());
-		newObj.setTitle(obj.getTitle());
-		newObj.setBoby(obj.getBoby());
-		newObj.setAuthor(obj.getAuthor());
-
-	}
-
-	public Post fromDTO(PostDTO objDto) {
-		return new Post(objDto.getId(), objDto.getDate(), objDto.getTitle(), objDto.getBoby(), objDto.getAuthor());
 	}
 }
